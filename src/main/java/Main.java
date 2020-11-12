ae9b9a103841be03ad997cbf26c60f6e952bde68
Emil Trangeled <emilbfh@hotmail.com>
2020-11-12 14:21:07 +0100

    Test names removed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,10 +4,5 @@ public class Main {
     public static void main(String[] args) {
         GUI gui = new GUI();
     }
-    //EMIL
-    //Christoffer
-    //Magnus
-    //Stig
-    // Mads
-    // Zahed
+
 }

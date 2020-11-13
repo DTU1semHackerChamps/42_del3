3c944df9c6a079c8d8385ad8cbd7d72755d26eb4
Chris2670 <70951742+Chris2670@users.noreply.github.com>
2020-11-13 12:35:21 +0100

    ChanceCardManager added

---
+++ src/main/java/ChanceCard/ChanceCardManager.java
@@ -0,0 +1,19 @@
+package ChanceCard;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
+public class ChanceCardManager {
+      private int card;
+
+      public card[] CardListInit(){
+          card[] cards = new card[3];
+
+
+
+
+    }
+
+}
--- src/main/java/ChanceCard/MoveCard.java
+++ src/main/java/ChanceCard/MoveCard.java
@@ -1,4 +1,38 @@
 package ChanceCard;
 
 public class MoveCard {
+    // 2. Ryk frem til start og modtag 2 penge
+    // 3. Ryk op til 5 felter frem
+    // 5. Ryk 1 felt frem eller tag et chancekort mere
+    // 11. Ryk frem til strandpromenaden
+
+    private int position;
+    private int balance;
+    private boolean drawcard;
+
+    public void Player(int balance, int position, boolean drawcard){
+
+        this.balance = balance;
+        this.position = position;
+        this.drawcard = drawcard;
+
+    }
+
+    public void movePlayer(){
+
+    }
+
+    public void addPlayerBalance(){
+
+    }
+    public void movePlayerSpecial(){
+
+    }
+    public void drawChanceCard(){
+
+    }
+    public void Strandpromenade(){
+
+    }
+
 }

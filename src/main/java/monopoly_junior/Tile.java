75c46580b2668ecc288e9476b951b70d46daedac
Zahed <zahedm45@gmail.com>
2020-11-18 12:50:47 +0100

    Chance cards have been removed from the array.

--- src/main/java/monopoly_junior/Tile.java
+++ src/main/java/monopoly_junior/Tile.java
@@ -7,8 +7,13 @@ public class Tile {
      * constructor
      */
     private int balanceChange;
-
     private String tileName;
+    private String propertyPrice;
+    public String propertyOwner;
+
+
+
+
 
 
     public Tile(String name,int balanceChange)
@@ -23,6 +28,11 @@ public class Tile {
     }
 
 
+    public String PropertyOwner (String playername, ){
+
+        return propertyOwner;
+
+    }
 
 
 
@@ -39,25 +49,22 @@ public class Tile {
         Tile start = new Tile ("START",2);
         Tile burgerbaren = new Tile ("BURGERBAREN", -1);
         Tile pizzeriaet = new Tile ("PIZZERIAET", -1);
-        Tile chanceKort1 = new Tile ("CHANCE", 0);
+        Tile chanceKort = new Tile ("CHANCE", 0);
         Tile slikButikken = new Tile ("SLIKBUTIKKEN", -1);
         Tile isKiosken = new Tile ("ISKIOSKEN", -1);
         Tile visitPrison = new Tile ("PÅ BESØG I FÆNGSEL", 0);
         Tile museet = new Tile ("MUSEET", -2);
         Tile biblioteket = new Tile ("BIBLIOTEKET", -2);
-        Tile chanceKort2 = new Tile ("CHANCE", 0);
         Tile skaterparken = new Tile ("SKATERPARKEN", -2);
         Tile swimmingpoolen = new Tile ("SWIMMINGPOOLEN",-2);
         Tile gratisParking = new Tile ("GRATIS PARKING",0);
         Tile spillehallen = new Tile ("SPILLEHALLEN", -3);
         Tile biografen = new Tile ("BIOGRAFEN", -3);
-        Tile chanceKort3 = new Tile ("CHANCE", 0);
         Tile toyStore = new Tile ("LEGTØJSBUTIKKEN", -3);
         Tile dyrehandlen = new Tile ("DYREHANDLEN", -3);
         Tile goPrison = new Tile ("GÅ I FÆNGSEL", 0);
         Tile bowlinghallen = new Tile ("BOWLINGHALLEN", -4);
         Tile zoo = new Tile ("ZOO", -4);
-        Tile chanceKort4 = new Tile ("CHANCE", 0);
         Tile vandlandet = new Tile ("VANDLANDET", -5);
         Tile strandpromenaden = new Tile ("STRANDPROMENADEN",-5);
         //
@@ -65,25 +72,25 @@ public class Tile {
         tiles[0] = start;
         tiles[1] = burgerbaren;
         tiles[2] = pizzeriaet;
-        tiles[3] = chanceKort1;
+        tiles[3] = chanceKort;
         tiles[4] = slikButikken;
         tiles[5] = isKiosken;
         tiles[6] = visitPrison;
         tiles[7] = museet;
         tiles[8] = biblioteket;
-        tiles[9] = chanceKort2;
+        tiles[9] = chanceKort;
         tiles[10] = skaterparken;
         tiles[11] = swimmingpoolen;
         tiles[12] = gratisParking;
         tiles[13] = spillehallen;
         tiles[14] = biografen;
-        tiles[15] = chanceKort3;
+        tiles[15] = chanceKort;
         tiles[16] = toyStore;
         tiles[17] = dyrehandlen;
         tiles[18] = goPrison;
         tiles[19] = bowlinghallen;
         tiles[20] = zoo;
-        tiles[21] = chanceKort4;
+        tiles[21] = chanceKort;
         tiles[22] = vandlandet;
         tiles[23] = strandpromenaden;
 

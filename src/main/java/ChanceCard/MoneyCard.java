9c6ce0579cc3fc91f471f68d2e335991ad0bc228
Spastinak <71653506+Spastinak@users.noreply.github.com>
2020-11-19 11:44:41 +0100

    added moneycard methode

--- src/main/java/ChanceCard/MoneyCard.java
+++ src/main/java/ChanceCard/MoneyCard.java
@@ -1,4 +1,18 @@
 package ChanceCard;
+import monopoly_junior.Player;
+import monopoly_junior.Tile;
 
 public class MoneyCard {
+
+    private int amount;
+
+    public useMoneyCard(int amount) {
+        this.amount = amount;
+    }
+
+    public void affect(Player currentPlayer, Player addBalance)
+    {
+        addBalance.addBalance(amount);
+    }
+
 }

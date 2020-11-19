dc758e02609e65abffa2490d257cd877330d75cb
Spastinak <71653506+Spastinak@users.noreply.github.com>
2020-11-19 10:26:13 +0100

    added jail card

--- src/main/java/ChanceCard/JailCard.java
+++ src/main/java/ChanceCard/JailCard.java
@@ -1,4 +1,11 @@
 package ChanceCard;
+import monopoly_junior.Player;
+import monopoly_junior.Tile;
 
 public class JailCard {
+
+    public void useJailCard(Tile[] tiles, Player currentPlayer){
+        JailCard = true;
+    }
+
 }

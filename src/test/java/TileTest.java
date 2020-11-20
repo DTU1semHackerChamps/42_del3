1ee8c769eb67928b483dc31c0864def608999d9e
Zahed <zahedm45@gmail.com>
2020-11-20 10:59:16 +0100

    Added Junit from maven version 5.4.2. Added separate test folder into the project.

---
+++ src/test/java/TileTest.java
@@ -0,0 +1,19 @@
+import monopoly_junior.Tile;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+public class TileTest {
+    // tests if the program correctly adds or subtracts the correct amount of points and if the extraturn rule works
+    @org.junit.jupiter.api.Test
+    void tileListInit() {
+        Tile[] tilesTest = new Tile[12];
+        tilesTest = Tile.tileListInit();
+        assertEquals(2,tilesTest[1].getBalanceChange());
+        assertEquals(-100,tilesTest[2].getBalanceChange());
+
+
+    }
+}
+

8f13106c4dcf55619f408d015b534c793f1f7728
Emil Trangeled <emilbfh@hotmail.com>
2020-11-24 21:02:46 +0100

    Started work on ChanceCardManager

---
+++ src/main/java/monopoly_junior/ChanceCardManager.java
@@ -0,0 +1,24 @@
+package monopoly_junior;
+
+import ChanceCard.*;
+
+public class ChanceCardManager {
+
+    public static ChanceCard[] cardArrayInit(){
+        ChanceCard[] chanceCards = new ChanceCard[12];
+
+        chanceCards[0] = new ChooseColorCard(3,4,3,2,1,3,5,2,4);
+
+    }
+
+    public static void cardShuffle(){
+
+    }
+
+    public static int drawCard(){
+
+        return card;
+    }
+
+
+}

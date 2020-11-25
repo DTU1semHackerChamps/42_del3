f2596deee5a1d14923946059f3aa8c2d08f6a14b
Emil Trangeled <emilbfh@hotmail.com>
2020-11-25 15:35:20 +0100

    Added drawCard method in ChanceCardManager

--- src/main/java/monopoly_junior/ChanceCardManager.java
+++ src/main/java/monopoly_junior/ChanceCardManager.java
@@ -10,30 +10,30 @@ import java.util.Random;
 
 public class ChanceCardManager {
 
-    public static ChanceCard[] cardArrayInit(Player currentPlayer, GUI gui) throws IOException {
+    public static ChanceCard[] cardArrayInit(Tile[] tiles, Player[] players, Player currentPlayer, GUI gui) throws IOException {
         ChanceCard[] chanceCards = new ChanceCard[20];
         HashMap<String,String> stringList = Language.languageInit("english");
 
         //chanceCards[0] = new CharacterCard;
-        chanceCards[1] = new moveMoneyCard(stringList.get("ChanceCard2"), null, null, currentPlayer, gui, 2, 0, true);
-        chanceCards[2] = new specificMoveCard(stringList.get("ChanceCard3"), null, null, currentPlayer, gui);
-        chanceCards[3] = new ColorCard(stringList.get("ChanceCard4"), null, null, currentPlayer, gui, 4, stringList.get("ColorOrange"));
-        //chanceCards[4] = new [1 skridt eller et kort metode](stringList.get("ChanceCard5"), null, null, currentPlayer, gui);
-        //chanceCards[5] = new CharacterCard(stringList.get("ChanceCard6"), null, null, currentPlayer, gui);
-        chanceCards[6] = new moveMoneyCard(stringList.get("ChanceCard7"), null, null, currentPlayer, gui, -2, 0, false);
-        chanceCards[7] = new ChooseColorCard(stringList.get("ChanceCard8"), null, null, currentPlayer, gui, 4,7, stringList.get("ColorOrange"), stringList.get("ColorGreen"));
-        chanceCards[8] = new ColorCard(stringList.get("ChanceCard9"), null, null, currentPlayer, gui, 2, stringList.get("ColorLightblue"));
-        chanceCards[9] = new JailCard(stringList.get("ChanceCard10"), null, null, currentPlayer, gui);
-        chanceCards[10] = new moveMoneyCard(stringList.get("ChanceCard11"), null, null, currentPlayer, gui, 0, 23, true);
-        //chanceCards[11] = new CharacterCard(stringList.get("ChanceCard12"), null, null, currentPlayer, gui);
-        //chanceCards[12] = new CharacterCard(stringList.get("ChanceCard13"), null, null, currentPlayer, gui);
-        //chanceCards[13] = new BirthdayCard(stringList.get("ChanceCard14"), null, null, currentPlayer, gui);
-        chanceCards[14] = new ChooseColorCard(stringList.get("ChanceCard15"), null, null, currentPlayer, gui, 3, 8, stringList.get("ColorPink"), stringList.get("ColorDarkblue"));
-        chanceCards[15] = new moveMoneyCard(stringList.get("ChanceCard16"), null, null, currentPlayer, gui, 2, 0, false);
-        chanceCards[16] = new ColorCard(stringList.get("ChanceCard17"), null, null, currentPlayer, gui, 5, stringList.get("ColorRed"));
-        chanceCards[17] = new ColorCard(stringList.get("ChanceCard18"), null, null, currentPlayer, gui, 9, stringList.get("SkateparkField"));
-        chanceCards[18] = new ChooseColorCard(stringList.get("ChanceCard19"), null, null, currentPlayer, gui, 2, 5, stringList.get("ColorLightblue"), stringList.get("ColorRed"));
-        chanceCards[19] = new ChooseColorCard(stringList.get("ChanceCard20"), null, null, currentPlayer, gui, 1, 6, stringList.get("ColorBrown"), stringList.get("ColorYellow"));
+        chanceCards[1] = new moveMoneyCard(stringList.get("ChanceCard2"), tiles, players, currentPlayer, gui, 2, 0, true);
+        chanceCards[2] = new specificMoveCard(stringList.get("ChanceCard3"), tiles, players, currentPlayer, gui);
+        chanceCards[3] = new ColorCard(stringList.get("ChanceCard4"), tiles, players, currentPlayer, gui, 4, stringList.get("ColorOrange"));
+        //chanceCards[4] = new [1 skridt eller et kort metode](stringList.get("ChanceCard5"), tiles, players, currentPlayer, gui);
+        //chanceCards[5] = new CharacterCard(stringList.get("ChanceCard6"), tiles, players, currentPlayer, gui);
+        chanceCards[6] = new moveMoneyCard(stringList.get("ChanceCard7"), tiles, players, currentPlayer, gui, -2, 0, false);
+        chanceCards[7] = new ChooseColorCard(stringList.get("ChanceCard8"), tiles, players, currentPlayer, gui, 4,7, stringList.get("ColorOrange"), stringList.get("ColorGreen"));
+        chanceCards[8] = new ColorCard(stringList.get("ChanceCard9"), tiles, players, currentPlayer, gui, 2, stringList.get("ColorLightblue"));
+        chanceCards[9] = new JailCard(stringList.get("ChanceCard10"), tiles, players, currentPlayer, gui);
+        chanceCards[10] = new moveMoneyCard(stringList.get("ChanceCard11"), tiles, players, currentPlayer, gui, 0, 23, true);
+        //chanceCards[11] = new CharacterCard(stringList.get("ChanceCard12"), tiles, players, currentPlayer, gui);
+        //chanceCards[12] = new CharacterCard(stringList.get("ChanceCard13"), tiles, players, currentPlayer, gui);
+        //chanceCards[13] = new BirthdayCard(stringList.get("ChanceCard14"), tiles, players, currentPlayer, gui);
+        chanceCards[14] = new ChooseColorCard(stringList.get("ChanceCard15"), tiles, players, currentPlayer, gui, 3, 8, stringList.get("ColorPink"), stringList.get("ColorDarkblue"));
+        chanceCards[15] = new moveMoneyCard(stringList.get("ChanceCard16"), tiles, players, currentPlayer, gui, 2, 0, false);
+        chanceCards[16] = new ColorCard(stringList.get("ChanceCard17"), tiles, players, currentPlayer, gui, 5, stringList.get("ColorRed"));
+        chanceCards[17] = new ColorCard(stringList.get("ChanceCard18"), tiles, players, currentPlayer, gui, 9, stringList.get("SkateparkField"));
+        chanceCards[18] = new ChooseColorCard(stringList.get("ChanceCard19"), tiles, players, currentPlayer, gui, 2, 5, stringList.get("ColorLightblue"), stringList.get("ColorRed"));
+        chanceCards[19] = new ChooseColorCard(stringList.get("ChanceCard20"), tiles, players, currentPlayer, gui, 1, 6, stringList.get("ColorBrown"), stringList.get("ColorYellow"));
 
         return chanceCards;
     }
@@ -54,9 +54,16 @@ public class ChanceCardManager {
 
     }
 
-    public static int drawCard(){
+    public static int drawCard(int index, ChanceCard[] chanceCards){
+        index++;
 
-        return card;
+        chanceCards[index].useChanceCard();
+
+        if(index > 20){
+            index = 0;
+        }
+
+        return index;
     }
 
 

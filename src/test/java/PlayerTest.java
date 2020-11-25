50258c9c393978af89599706b38a0610f41deb28
Zahed <zahedm45@gmail.com>
2020-11-25 14:03:28 +0100

    Some stuff was missing from Player_branch

---
+++ src/test/java/PlayerTest.java
@@ -0,0 +1,57 @@
+import monopoly_junior.Player;
+
+import static org.junit.Assert.*;
+
+
+public class PlayerTest {
+
+    Player player = new Player(2,0, "Neilsen", 4);
+
+
+    @org.junit.Test
+    public void setJailCard() {
+
+
+
+    }
+
+    @org.junit.Test
+    public void isJailCard() {
+    }
+
+    @org.junit.Test
+    public void setBalance() {
+    }
+
+    @org.junit.Test
+    public void getBalance() {
+    }
+
+    @org.junit.Test
+    public void addBalance() {
+    }
+
+    @org.junit.Test
+    public void setPosition() {
+    }
+
+    @org.junit.Test
+    public void getPosition() {
+    }
+
+    @org.junit.Test
+    public void addPosition() {
+    }
+
+    @org.junit.Test
+    public void hasWon() {
+    }
+
+    @org.junit.Test
+    public void setPlayerName() {
+    }
+
+    @org.junit.Test
+    public void getPlayerName() {
+    }
+}
